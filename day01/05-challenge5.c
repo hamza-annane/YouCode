9d6c182300ee5811f65ab69052834b1f36388a26
Th3Hacker <hamza.annane.official@gmail.com>
2025-07-21 20:11:46 +0100

    day01

--- day01/05-challenge5.c
+++ day01/05-challenge5.c
@@ -24,3 +24,47 @@
 **   - Based on the user's choice, perform the appropriate conversion
 **     and display the result.
 */
+
+#include <stdio.h>
+
+int main()
+{
+    int years;
+    int option;
+
+    printf("Enter a number of years: ");
+    scanf("%d", &years);
+
+    printf("\nChoose a conversion option:\n");
+    printf("1. Convert to Months\n");
+    printf("2. Convert to Days\n");
+    printf("3. Convert to Hours\n");
+    printf("4. Convert to Minutes\n");
+    printf("5. Convert to Seconds\n");
+    printf("Enter your choice (1-5): ");
+    scanf("%d", &option);
+
+    switch (option)
+    {
+        case 1:
+            printf("%d years = %d months\n", years, years * 12);
+            break;
+        case 2:
+            printf("%d years = %d days\n", years, years * 365);
+            break;
+        case 3:
+            printf("%d years = %d hours\n", years, years * 365 * 24);
+            break;
+        case 4:
+            printf("%d years = %d minutes\n", years, years * 365 * 24 * 60);
+            break;
+        case 5:
+            printf("%d years = %d seconds\n", years, years * 365 * 24 * 60 * 60);
+            break;
+        default:
+            printf("Invalid choice. Please run the program again and choose between 1 and 5.\n");
+            break;
+    }
+
+    return 0;
+}
