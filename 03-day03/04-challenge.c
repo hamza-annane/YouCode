3bf45038834d1c4d304924844caf63cd1dc0652f
Th3Hacker <hamza.annane.official@gmail.com>
2025-07-25 21:38:01 +0100

    Challenge 4 : Affichage des N Premiers Nombres Impairs

--- 03-day03/04-challenge.c
+++ 03-day03/04-challenge.c
@@ -9,10 +9,7 @@ int     main()
 
     for(int i = 1 ; i <= n ; i++)
     {
-        if (i % 2 != 0)
-        {
-            printf(" %d ",i);
-        }
+        printf(" %d ",i * 2 - 1);
     }
     
     printf("\n");
